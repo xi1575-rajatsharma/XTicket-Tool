64d6661a3c5e86c63bf64b6480c9e77172064d00
xi1575-rajatsharma <rjrajat1443@gmail.com>
2021-07-05 10:19:36 +0530

    Removed Comments

--- src/core/EmployeeDasboard/EmployeeDashboardReports/EmployeeDasboardReports.js
+++ src/core/EmployeeDasboard/EmployeeDashboardReports/EmployeeDasboardReports.js
@@ -10,20 +10,6 @@ const EmployeeDasboardReports = (props) => {
 
   const mapChangesToState = (value) => setState({ ...state, ...value });
 
-  // const setUser = (selectedUser) => {
-  //   const requestParams = {
-  //     email: selectedUser.value,
-  //     startDate: "2021-06-20",
-  //     endDate: "",
-  //   };
-  //   props.dispatch(actionCreators.resetEmployeeTicketStatusCount());
-  //   props.dispatch(actionCreators.startEmployeeTicketStatusCountLoader());
-  //   props.dispatch(actionCreators.getEmployeeTicketStatusCount(requestParams));
-  //   props.dispatch(actionCreators.resetEmployeeSlaInfo());
-  //   props.dispatch(actionCreators.startEmployeeSlaInfoLoader());
-  //   props.dispatch(actionCreators.getEmployeeSlaInfo(requestParams));
-  //   mapChangesToState({ selectedUser });
-  // };
   return (
     <styled.container>
       {/* <EmployeeDashboardReportsHeader /> */}
